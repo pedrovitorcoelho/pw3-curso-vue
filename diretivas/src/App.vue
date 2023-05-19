e5108b6b1c70f8627343df863d49f18a11de6f27
Higor Moroni <moroni.higor@gmail.com>
2023-05-18 22:55:32 -0300

    feat: add directives content

--- diretivas/src/App.vue
+++ diretivas/src/App.vue
@@ -1,12 +1,64 @@
 <template>
 	<div id="app">
 		<h1>Diretivas</h1>
+		<hr>
+		<p v-html="'Usando diretiva <strong>v-html</strong>'" ></p>
+		<p v-text="'Usando diretiva <strong>v-text</strong>'" ></p>
+
+		<p v-destaque:fundo.atrasar="'lightblue'" > Usando diretiva personalizada </p>
+		<p v-destaque.atrasar="'pink'"> Usando diretiva personalizada </p>
+		<p v-destaque:fundo.atrasar.alternar="destaqueConfigs"> Usando diretiva personalizada </p>
+		<!-- v-model.trim.lazy.number="data" -->
+		<!-- v-bind:key="data" -->
 	</div>
 </template>
 
 <script>
 export default {
-	
+	directives: {
+		destaque: {
+			bind(el, binding, vnode) {
+				//el.style.backgroundColor = binding.value;
+				const aplicarCor = cor => {
+					if(binding.arg === 'fundo'){
+						el.style.backgroundColor = cor
+					} else {
+						el.style.color = cor
+					}
+
+					let atraso = 0;
+					if(binding.modifiers['atrasar']){
+						atraso = binding.value.atraso;
+					}
+
+					const cor1 = binding.value.cor1;
+					const cor2 = binding.value.cor2;
+					let corAtual = cor1;
+
+					setTimeout(() => {
+						if(binding.modifiers['alternar']) {
+							setInterval(() => {
+								corAtual = corAtual === cor1 ? cor2 : cor1
+								aplicarCor(corAtual);
+							}, binding.value.intervalo)
+						} else {
+							aplicarCor(cor1);	
+						}
+					}, atraso);
+				}
+			}
+		}
+	},
+	data() {
+		return {
+			destaqueConfigs: {
+				cor1: 'green',
+				cor2: 'red',
+				atraso: 2000,
+				intervalo: 200
+			}
+		}
+	}
 }
 </script>
 
